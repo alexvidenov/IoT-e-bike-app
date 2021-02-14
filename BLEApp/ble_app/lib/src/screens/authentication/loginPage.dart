a78d6738bb5a323b90481d3f584dccb7397d445d
Parakatowski <a.videnov8a@gmail.com>
2021-02-14 18:11:48 +0200

    Adding decent image in the login page.

--- BLEApp/ble_app/lib/src/screens/authentication/loginPage.dart
+++ BLEApp/ble_app/lib/src/screens/authentication/loginPage.dart
@@ -2,64 +2,6 @@ import 'package:ble_app/src/blocs/authBloc.dart';
 import 'package:ble_app/src/sealedStates/authState.dart';
 import 'package:flutter/material.dart';
 
-const String title = "Zecotec";
-const String noAccount = "Don't have an account?";
-const Color kMainColor = Color(0xffFFFFFF);
-const Color kSecondaryColor = Colors.lightBlueAccent;
-
-Padding inputWidget(
-    {String hintText,
-    IconData primaryIcon,
-    bool endIcon = false,
-    IconData sufixIconData,
-    void Function(String) onChanged}) {
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: TextField(
-      decoration: InputDecoration(
-        suffixIcon: endIcon
-            ? Icon(
-                sufixIconData,
-                color: kMainColor,
-              )
-            : null,
-        hintText: hintText,
-        hintStyle: TextStyle(color: kMainColor),
-        icon: Icon(
-          primaryIcon,
-          color: kMainColor,
-        ),
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: kMainColor.withOpacity(0.8), width: 1),
-        ),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: kMainColor, width: 1.5),
-        ),
-      ),
-      onChanged: onChanged,
-    ),
-  );
-}
-
-Widget mainButton(IconData iconName, Function onPressed) {
-  return Padding(
-    padding: const EdgeInsets.only(top: 20.0),
-    child: SizedBox(
-      width: 180,
-      height: 45,
-      child: FlatButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          color: kSecondaryColor,
-          onPressed: onPressed,
-          child: Icon(
-            iconName,
-            color: kMainColor,
-          )),
-    ),
-  );
-}
-
 class LoginScreen extends StatefulWidget {
   final AuthBloc _auth;
   final Function toggleView;
@@ -119,6 +61,57 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+  Padding _buildInput(
+          {String hintText,
+          IconData primaryIcon,
+          bool endIcon = false,
+          IconData suffixIconData,
+          void Function(String) onChanged}) =>
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: InputDecoration(
+            suffixIcon: endIcon
+                ? Icon(
+                    suffixIconData,
+                    color: Colors.white,
+                  )
+                : null,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.white),
+            icon: Icon(
+              primaryIcon,
+              color: Colors.white,
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide:
+                  BorderSide(color: Colors.white.withOpacity(0.8), width: 1),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 1.5),
+            ),
+          ),
+          onChanged: onChanged,
+        ),
+      );
+
+  Widget _buildLoginButton(IconData iconName, Function onPressed) => Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: SizedBox(
+          width: 180,
+          height: 45,
+          child: FlatButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              color: Colors.lightBlueAccent,
+              onPressed: onPressed,
+              child: Icon(
+                iconName,
+                color: Colors.white,
+              )),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -145,21 +138,20 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Stack(
           fit: StackFit.expand,
           children: <Widget>[
-            Image.network(
-              "https://cdn.mos.cms.futurecdn.net/62L9uRGHNzpjYzvMEseSYH.jpg",
-              // TODO: replace with some other image, this is test
+            Image(
+              image: AssetImage('assets/images/login_image.jpg'),
               fit: BoxFit.fill,
             ),
             Container(
-              color: kSecondaryColor.withOpacity(0.6),
+              color: Colors.lightBlueAccent.withOpacity(0.6),
             ),
             SingleChildScrollView(
               child: Form(
-                key: this.formKey,
+                key: formKey,
                 child: Column(
                   children: <Widget>[
                     Container(
-                        height: height * 0.4,
+                        height: height * 0.5,
                         alignment: Alignment.center,
                         child: RichText(
                           textAlign: TextAlign.center,
@@ -167,7 +159,8 @@ class _LoginScreenState extends State<LoginScreen> {
                               text: 'Z',
                               style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: 35,
+                                  fontSize: 40,
+                                  fontStyle: FontStyle.italic,
                                   fontFamily: 'Europe_Ext'),
                               children: [
                                 TextSpan(
@@ -186,30 +179,31 @@ class _LoginScreenState extends State<LoginScreen> {
                                 ),
                               ]),
                         )),
-                    inputWidget(
+                    _buildInput(
                         hintText: "Email",
                         primaryIcon: Icons.person_outline,
                         onChanged: (value) => this._email = value),
-                    inputWidget(
+                    _buildInput(
                         hintText: "Password",
                         primaryIcon: Icons.vpn_key,
                         endIcon: true,
-                        sufixIconData: Icons.help_outline,
+                        suffixIconData: Icons.help_outline,
                         onChanged: (value) => this._password = value),
-                    mainButton(Icons.exit_to_app, () => validateAndSubmit()),
+                    _buildLoginButton(
+                        Icons.exit_to_app, () => validateAndSubmit()),
                     Align(
                       alignment: Alignment.bottomCenter,
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          Text(noAccount,
+                          Text("Don't have an account?",
                               style:
-                                  TextStyle(color: kMainColor, fontSize: 16)),
+                                  TextStyle(color: Colors.white, fontSize: 16)),
                           FlatButton(
                               onPressed: () => widget.toggleView(),
                               child: Text(
                                 "SignUp",
-                                style: TextStyle(color: kMainColor),
+                                style: TextStyle(color: Colors.white),
                               ))
                         ],
                       ),
